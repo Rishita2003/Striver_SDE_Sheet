4ea7e36008e2a430a02b97cbe2c775f0eef236e7
MALLELA VENKATA SATYA RISHITA <89648582+Rishita2003@users.noreply.github.com>
2023-06-07 18:57:43 +0530

    Create missing_repeating.py

---
+++ Day2/missing_repeating.py
@@ -0,0 +1,39 @@
+'''
+Problem Statement: You are given a read-only array of N integers with values also in the range [1, N] both inclusive. 
+Each integer appears exactly once except A which appears twice and B which is missing. 
+The task is to find the repeating and missing numbers A and B where A repeats twice and B is missing.
+
+Example 1:
+
+Input Format:  array[] = {3,1,2,5,3}
+
+Result: {3,4)
+
+Explanation: A = 3 , B = 4 
+Since 3 is appearing twice and 4 is missing
+Example 2:
+
+Input Format: array[] = {3,1,2,5,4,6,7,5}
+
+Result: {5,8)
+
+Explanation: A = 5 , B = 8 
+Since 5 is appearing twice and 8 is missing
+'''
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+def missingAndRepeating(arr, n):
+    # Write your code here
+    l=[]
+    for i in arr:
+        if i not in l:
+            l.append(i)
+    rep=sum(arr)-sum(l)
+    ts=n*(n+1)//2
+    s=sum(set(arr))
+    miss=abs(s-ts)
+    return miss,rep
+    pass
